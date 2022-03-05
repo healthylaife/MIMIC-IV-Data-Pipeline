140a776ea47300e860958f4b8915feab4f2ec919
bgallamoza <brennan.gallamoza@gmail.com>
2022-03-04 20:54:04 -0500

    added logic for creating readmission labels based on discharge time instead of admittime

--- preprocessing/day_intervals_preproc/day_intervals_cohort.py
+++ preprocessing/day_intervals_preproc/day_intervals_cohort.py
@@ -8,8 +8,8 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + './..')
 TESTING = False # Variable for testing functions
 OUTPUT_DIR = './data/day_intervals/cohort'
 
-def get_adm_pts(mimic4_path:str, use_ICD:False):
-    adm = pd.read_csv(mimic4_path + "core/admissions.csv.gz", compression='gzip', header=0, index_col=None, parse_dates=['admittime'])
+def get_adm_pts(mimic4_path:str):
+    adm = pd.read_csv(mimic4_path + "core/admissions.csv.gz", compression='gzip', header=0, index_col=None, parse_dates=['admittime', 'dischtime'])
     adm = adm.loc[adm.hospital_expire_flag == 0]    # remove hospitalizations with a death; impossible for readmission for such visits
 
     pts = pd.read_csv(
@@ -21,48 +21,47 @@ def get_adm_pts(mimic4_path:str, use_ICD:False):
     # Define anchor_year corresponding to the anchor_year_group 2017-2019. This is later used to prevent consideration
     # of visits with prediction windows outside the dataset's time range (2008-2019)
 
-    adm_pts = adm[['subject_id', 'hadm_id', 'admittime']].merge(
+    adm_pts = adm[['subject_id', 'hadm_id', 'admittime', 'dischtime']].merge(
             pts[['subject_id', 'anchor_year', 'anchor_age', 'yob', 'min_valid_year']], how='inner', left_on='subject_id', right_on='subject_id'
         )
     adm_pts = adm_pts.loc[adm_pts['admittime'].dt.year - adm_pts['yob'] >= 18]
-    # adm_pts['max_valid_year'] = get_range(adm_pts, 'admittime', 'anchor_year', measure='years', keep_lower=True)
 
-    return adm_pts.dropna(subset=['min_valid_year'])[['subject_id', 'hadm_id', 'admittime', 'min_valid_year']]
+    return adm_pts.dropna(subset=['min_valid_year'])[['subject_id', 'hadm_id', 'admittime', 'dischtime', 'min_valid_year']]
 
 
-def is_row_valid(row, gap, time_col, valid_col, max_year):
-    pred_year = (row[time_col] + gap).year
-    if pred_year > row[valid_col] and max_year < pred_year:
+def is_row_valid(row, gap, disch_col, valid_col, max_year):
+    pred_year = (row[disch_col] + gap).year
+    if max_year < pred_year and pred_year > row[valid_col]:
         return False
     return True
 
 
-def get_case_ctrls(df:pd.DataFrame, gap:int, group_col:str, visit_col:str, time_col:str, valid_col:str) -> pd.DataFrame:
+def get_case_ctrls(df:pd.DataFrame, gap:int, group_col:str, visit_col:str, admit_col:str, disch_col:str, valid_col:str) -> pd.DataFrame:
     case = pd.DataFrame()   # hadm_ids with readmission within the gap period
     ctrl = pd.DataFrame()   # hadm_ids without readmission within the gap period
     invalid = pd.DataFrame()    # hadm_ids that are not considered in the cohort
     gap = datetime.timedelta(days=gap)  # transform gap into a timedelta to compare with datetime columns
 
     def validate_row(row, ctrl, invalid, max_year):
-        if is_row_valid(row, gap, time_col, valid_col, max_year):
+        if is_row_valid(row, gap, disch_col, valid_col, max_year):
             ctrl = ctrl.append(row)
         else:
             invalid = invalid.append(row)
         return ctrl, invalid
 
-    # Iterate through groupbys based on group_col (subject_id). Data is sorted by subject_id and time_col (admittime)
+    # Iterate through groupbys based on group_col (subject_id). Data is sorted by subject_id and admit_col (admittime)
     # to ensure that the most current hadm_id is last in a group.
-    for subject, group in df[[group_col, visit_col, time_col, valid_col]].sort_values(by=[group_col, time_col]).groupby(group_col):
-        max_year = group.max()[time_col].year
+    for subject, group in df[[group_col, visit_col, admit_col, disch_col, valid_col]].sort_values(by=[group_col, admit_col]).groupby(group_col):
+        max_year = group.max()[disch_col].year
 
         if group.shape[0] <= 1:
             ctrl, invalid = validate_row(group.iloc[0], ctrl, invalid, max_year)   # A group with 1 row has no readmission; goes to ctrl
         else:
             for idx in range(group.shape[0]-1):
-                visit_time = group.iloc[idx][time_col]  # For each index (a unique hadm_id), get its timestamp
+                visit_time = group.iloc[idx][disch_col]  # For each index (a unique hadm_id), get its timestamp
                 if group.loc[
-                    (group[time_col] > visit_time) &    # Readmissions must come AFTER the current timestamp
-                    (group[time_col] - visit_time <= gap)   # Distance between a timestamp and readmission must be within gap
+                    (group[admit_col] > visit_time) &    # Readmissions must come AFTER the current timestamp
+                    (group[admit_col] - visit_time <= gap)   # Distance between a timestamp and readmission must be within gap
                     ].shape[0] >= 1:                # If ANY rows meet above requirements, a readmission has occurred after that visit
 
                     case = case.append(group.iloc[idx])
@@ -75,9 +74,9 @@ def get_case_ctrls(df:pd.DataFrame, gap:int, group_col:str, visit_col:str, time_
                     ctrl = ctrl.append(group.iloc[idx])
 
             ctrl, invalid = validate_row(group.iloc[-1], ctrl, invalid, max_year)  # The last hadm_id datewise is guaranteed to have no readmission logically
-            print(f"{case.shape[0] + ctrl.shape[0]}/{df.shape[0]} hadm_ids processed")
+            print(f"[ {gap.days} DAYS ] {case.shape[0] + ctrl.shape[0]}/{df.shape[0]} hadm_ids processed")
 
-    print(f"{invalid.shape[0]} hadm_ids are invalid")
+    print(f"[ {gap.days} DAYS ] {invalid.shape[0]} hadm_ids are invalid")
     # case hadm_ids are labelled 1 for readmission, ctrls have a 0 label
     case['label'] = np.ones(case.shape[0]).astype(int)
     ctrl['label'] = np.zeros(ctrl.shape[0]).astype(int)
@@ -95,24 +94,33 @@ def test_case_ctrls(test_case=True, df=None, df_new=None, invalid=None):
                 datetime.datetime(year=2000, month=1 ,day=1) + datetime.timedelta(days=i) for i in [
                     1,      # Test that group size of 1 automatically sends to ctrl
                     1, 5,   # Ensures that the last visit is always ctrl (given that its within the min valid year)
-                    1, 3, 5, 60, 20, 100, 1000, # Tests reordering
-                    1, 350, 370,    # Ensures that readmissions extending beyond the minimum valid year
-                    1, 360     #
+                    1, 3, 5, 60, 20, 100, 1000, # Tests reordering and that gaps are appropriately sent to ctrl
+                    1, 350, 357,    # Ensures that readmissions extending beyond the minimum valid year are removed
+                    1, 43     # Test to make sure readmissions are based on the end of disch_time, NOT admit_time
+                ]
+            ],
+            'dischtime': [
+                datetime.datetime(year=2000, month=1 ,day=1) + datetime.timedelta(days=i) for i in [
+                    2,      # Test that group size of 1 automatically sends to ctrl
+                    2, 7,   # Ensures that the last visit is always ctrl (given that its within the min valid year)
+                    2, 4, 6, 62, 22, 103, 1003, # Tests reordering
+                    2, 353, 360,    # Ensures that readmissions extending beyond the minimum valid year are removed
+                    40, 46     # Test to make sure readmissions are based on the end of disch_time, NOT admit_time
                 ]
             ],
             'min_valid_year':[2020]*10 + [2000]*5
-            # 'yob': [1980]*10
         })
-        df_new, invalid = get_case_ctrls(df, 30, 'subject_id', 'hadm_id', 'admittime', 'min_valid_year') #, 'yob'
+        df_new, invalid = get_case_ctrls(df, 30, 'subject_id', 'hadm_id', 'admittime', 'dischtime', 'min_valid_year') #, 'yob'
 
-        assert df_new.loc[df_new.label == 1].shape[0] == 5, f"New df has {df_new.loc[df_new.label == 1].shape[0]} readmits"
-        assert df_new.loc[df_new.label == 0].shape[0] == 9, f"New df has {df_new.loc[df_new.label == 0].shape[0]} ctrls"
+        assert df_new.loc[df_new.label == 1].shape[0] == 6, f"New df has {df_new.loc[df_new.label == 1].shape[0]} readmits"
+        assert df_new.loc[df_new.label == 0].shape[0] == 8, f"New df has {df_new.loc[df_new.label == 0].shape[0]} ctrls"
 
     assert df.shape[0] == df_new.shape[0] + invalid.shape[0]    # Ensure sizes are did not change
     assert set(df.subject_id.unique()) == set(np.concatenate((df_new.subject_id.unique(), invalid.subject_id.unique())))    # Check that subject_ids are identical
     assert df.hadm_id.nunique() == df_new.hadm_id.nunique() + invalid.hadm_id.nunique() # Check that all hadm_ids remained
 
     print("All tests passed!")
+
 
 def extract(cohort_output:str, summary_output:str, use_ICU:bool, interval:int, label:str):
     """
@@ -131,16 +139,35 @@ def extract(cohort_output:str, summary_output:str, use_ICU:bool, interval:int, l
     if use_ICU:
         pass
     else:
-        cohort, invalid = get_case_ctrls(pts, interval, 'subject_id', 'hadm_id', 'admittime', 'min_valid_year')
+        cohort, invalid = get_case_ctrls(pts, interval, 'subject_id', 'hadm_id', 'admittime', 'dischtime','min_valid_year')
     # test_case_ctrls(test_case=False, df=pts, df_new=cohort, invalid=invalid)
     cohort.to_csv(f"{cohort_output}.csv.gz", index=False, compression='gzip')
+
 
 if __name__ == '__main__':
     if TESTING:
         test_case_ctrls()
     else:
+        output_info = []
         adm_pts = get_adm_pts("./mimic-iv-1.0/")
         for i in [30, 60, 90]:
-            cohort, invalid = get_case_ctrls(adm_pts, i, 'subject_id', 'hadm_id', 'admittime', 'min_valid_year')
+            cohort, invalid = get_case_ctrls(
+                df=adm_pts,
+                gap=i,
+                group_col='subject_id',
+                visit_col='hadm_id',
+                admit_col='admittime',
+                disch_col='dischtime',
+                valid_col='min_valid_year'
+                )
             test_case_ctrls(test_case=False, df=adm_pts, df_new=cohort, invalid=invalid)
             cohort.to_csv(f"{OUTPUT_DIR}/cohort{i}day.csv.gz", index=False, compression='gzip')
+
+            output_info.append((i, cohort.shape[0], invalid.shape[0], cohort.label.value_counts()))
+
+        for info in output_info:
+            print(f"{info[0]} DAY GAP")
+            print("Rowsize of dataset: ", info[1])
+            print("Number of invalid hadm_ids: ", info[2])
+            print(info[3])
+            print("====================")
