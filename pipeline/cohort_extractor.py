56348ba2e3442d8addebe0ece75ba22d2e60babc
mathias-samuelides <mathias.samuelides@gmail.com>
2023-12-08 13:24:24 +0100

    docstring

--- pipeline/cohort_extractor.py
+++ pipeline/cohort_extractor.py
@@ -1,6 +1,5 @@
-from pathlib import Path
+from typing import Tuple
 import pandas as pd
-import datetime
 import logging
 from pipeline.file_info.raw.hosp import (
     load_hosp_patients,
@@ -8,9 +7,8 @@ from pipeline.file_info.raw.hosp import (
     HospAdmissions,
 )
 from pipeline.file_info.raw.icu import load_icu_icustays
-from pipeline.file_info.preproc.cohort import COHORT_PATH, CohortHeader
-from pipeline.prediction_task import PredictionTask, TargetType
-
+from pipeline.file_info.preproc.cohort import CohortHeader
+from pipeline.prediction_task import PredictionTask
 from pipeline.preprocessing.visit import (
     make_patients,
     make_icu_visits,
@@ -24,18 +22,28 @@ logger = logging.getLogger()
 
 
 class CohortExtractor:
+    """
+    Extracts cohort data based on specified prediction tasks and ICU status.
+
+    Attributes:
+        prediction_task (PredictionTask): The prediction task to be used for cohort extraction.
+        cohort_output (Path): The path for the output of the cohort data.
+    """
+
     def __init__(
         self,
         prediction_task: PredictionTask,
-        cohort_output: Path = None,
+        cohort_output: str = None,
     ):
         self.prediction_task = prediction_task
         self.cohort_output = cohort_output
 
     def get_icu_status(self) -> str:
+        """Determines the ICU status based on the prediction task."""
         return "ICU" if self.prediction_task.use_icu else "Non-ICU"
 
     def generate_extract_log(self) -> str:
+        """Generates a log message for the extraction process."""
         icu_log = self.get_icu_status()
         task_info = f"{icu_log} | {self.prediction_task.target_type}"
         if self.prediction_task.disease_readmission:
@@ -47,6 +55,7 @@ class CohortExtractor:
         return f"EXTRACTING FOR: {task_info} | {self.prediction_task.nb_days} |".upper()
 
     def generate_output_suffix(self) -> str:
+        """Generates a suffix for the output file based on the task details."""
         return (
             self.get_icu_status()  # .lower()
             + "_"
@@ -62,6 +71,7 @@ class CohortExtractor:
         )
 
     def fill_outputs(self) -> None:
+        """Fills in the output details based on the prediction task."""
         disease_selection = (
             f"_{self.prediction_task.disease_selection}"
             if self.prediction_task.disease_selection
@@ -72,7 +82,8 @@ class CohortExtractor:
             or f"cohort_{self.generate_output_suffix()}{disease_selection}"
         )
 
-    def load_hospital_data(self):
+    def load_hospital_data(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """Loads hospital patient and admission data."""
         return load_hosp_patients(), load_hosp_admissions()
 
     def create_visits(self, hosp_patients, hosp_admissions):
@@ -84,7 +95,13 @@ class CohortExtractor:
         else:
             return make_no_icu_visits(hosp_admissions, self.prediction_task.target_type)
 
-    def filter_and_merge_visits(self, visits, hosp_patients, hosp_admissions):
+    def filter_and_merge_visits(
+        self,
+        visits: pd.DataFrame,
+        hosp_patients: pd.DataFrame,
+        hosp_admissions: pd.DataFrame,
+    ) -> pd.DataFrame:
+        """Filters and merges visit records with patient and admission data."""
         visits = filter_visits(
             visits,
             self.prediction_task.disease_readmission,
@@ -104,6 +121,12 @@ class CohortExtractor:
         return visits
 
     def extract(self) -> Cohort:
+        """
+        Extracts the cohort data based on specified criteria and saves it.
+
+        Returns:
+            Cohort: The extracted and processed cohort data.
+        """
         logger.info("===========MIMIC-IV v2.0============")
         self.fill_outputs()
         logger.info(self.generate_extract_log())
